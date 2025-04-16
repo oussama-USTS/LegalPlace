99e39c0b24922ae7a7ad2ad1c50074552155e786
oussama-USTS <oussama@usts.fr>
2025-04-16 10:17:16 +0200

    config :docker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  output: 'standalone',
+  images: {
+    domains: ['images.unsplash.com', 'randomuser.me'],
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 }
 
 module.exports = nextConfig 
